1ede99863158b86275fb2662a1e5218413fd8b98
Ryan Hodin <ryan951357@gmail.com>
2017-04-30 03:38:24 -0500

    Correctly implement background fades without background-image transitions - Fixes #50

--- public_html/js/theme-changer.js
+++ public_html/js/theme-changer.js
@@ -1,51 +1,66 @@
 function selectChicagoEvening() {
-  
+
   var selected=document.getElementById("selected-css");
   var current=selected.href; // The URL of the currently selected theme
   current=current.substring(current.lastIndexOf('/')+1); // ...Just the filename
-  
+
   // Set the transition CSS file to be that filename in the appropriate folder
   document.getElementById("transition-css").href="/css/themes/transition/"+current;
- 
+
+  selected.href=""
+
   // And set a callback to begin the transition in 16ms (one frame at 60FPS)
   setTimeout(function() {
     document.getElementById("selected-css").href = "/css/themes/chicago-evening.css";
     document.getElementById("subtitle").innerHTML = "A Rhythmic Experience";
     localStorage.setItem('themeKey', 'chicagoEvening');
+
+    // And remove the transition
+    setTimeout(function() {document.getElementById("transition-css").href=""}, 500)
   }, 16);
 }
 
 function selectCyberpunkBartender() {
-  
+
   var selected=document.getElementById("selected-css");
   var current=selected.href; // The URL of the currently selected theme
   current=current.substring(current.lastIndexOf('/')+1); // ...Just the filename
-  
+
   // Set the transition CSS file to be that filename in the appropriate folder
   document.getElementById("transition-css").href="/css/themes/transition/"+current;
- 
+
+  selected.href=""
+
   // And set a callback to begin the transition in 16ms (one frame at 60FPS)
   setTimeout(function() {
     document.getElementById("selected-css").href = "/css/themes/cyberpunk-bartender.css";
     document.getElementById("subtitle").innerHTML = "A Retro Cyberpunk Jukebox";
     localStorage.setItem('themeKey', 'cyberpunkBartender');
+
+    // And remove the transition
+    setTimeout(function() {document.getElementById("transition-css").href=""}, 500)
   }, 16);
 }
 
 function selectMayberry() {
-  
+
   var selected=document.getElementById("selected-css");
   var current=selected.href; // The URL of the currently selected theme
   current=current.substring(current.lastIndexOf('/')+1); // ...Just the filename
-  
+
   // Set the transition CSS file to be that filename in the appropriate folder
   document.getElementById("transition-css").href="/css/themes/transition/"+current;
- 
+
+  selected.href=""
+
   // And set a callback to begin the transition in 16ms (one frame at 60FPS)
   setTimeout(function() {
     document.getElementById("selected-css").href = "/css/themes/mayberry.css";
     document.getElementById("subtitle").innerHTML = "A Rhythmic Ξxperience";
     localStorage.setItem('themeKey', 'mayberry');
+
+    // And remove the transition
+    setTimeout(function() {document.getElementById("transition-css").href=""}, 500)
   }, 16);
 }
 
