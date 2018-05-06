4e6d5786b10f910038e1bc0272028c1926e5d641
Ryan Hodin <ryan951357@gmail.com>
2018-05-06 04:21:09 +0000

    Remove radio code
    Leaving in the playbutton toggling code (modified to work without a stream)

--- public/js/radio-page.js
+++ public/js/radio-page.js
@@ -18,73 +18,16 @@ function defaultPlayer() {
   // Selects either the localstorage volume or a default value
   var vol = localStorage.getItem('volumeKey') || 0.77;
   document.getElementById("volume").value = vol;
-  var volume = document.getElementById("stream");
-  volume.volume = vol;
 }
-
-// GETS and displays currently playing info
-function radioTitle() {
-  // Located on Testament's stream client 'web' folder
-  var url = 'http://cadenceradio.com:8000/now-playing.xsl';
-
-  $.ajax({
-    type: 'GET',
-    url: url,
-    async: true,
-    jsonpCallback: 'parseMusic',
-    contentType: "application/json",
-    dataType: 'jsonp',
-    success: function (json) {
-      // Grab and trim song data
-      var nowPlayingArtist = json['/cadence1']['artist_name'].trim();
-      var nowPlayingSong = json['/cadence1']['song_title'].trim();
-      
-      // Player box
-      $('#artist_name').text(nowPlayingArtist);
-      $('#song_title').text(nowPlayingSong);
-      
-      // Title
-      window.document.title =  "CR♥ | " + nowPlayingArtist + " - " + nowPlayingSong;
-    },
-    error: function (e) {
-      console.log(e.message);
-      document.getElementById("artist_name").innerHTML = "<span style='color: red;'>Unable to load track data. <br/> :(</span>";
-      document.getElementById("song_title").innerHTML = "";
-    }
-  });
-}
-
-// Update now playing info at an interval
-$(document).ready(function () {
-  setTimeout(function () {
-    radioTitle();
-  }, 0);
-  setInterval(function () {
-    radioTitle();
-  }, 10000);
-});
 
 // Play/pause button
 $(document).ready(function () {
   document.getElementById("playerToggle").addEventListener('click', function(){
   // Play/pause button
-  var stream = document.getElementById("stream");
-  var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
-
-  if (stream.paused) {
-    // Loads up the real stream again if mobile
-    if (mobile) {
-      stream.src = "http://cadenceradio.com:8000/cadence1";
-    }
-    stream.load();
-    stream.play();
+  var toggle = document.getElementById("playerToggle");
+  if (toggle.innerHTML!="❚❚") {
     document.getElementById("playerToggle").innerHTML = "❚❚";
   } else {
-    // Loads up nothing if mobile
-    if (mobile) {
-      stream.src = "";
-    }
-    stream.load();
     document.getElementById("playerToggle").innerHTML = "►";
   }
 }, true)
@@ -103,9 +46,6 @@ $(document).ready(function () {
 
 // When you change the volume
 function volumeToggle(vol) {
-  var volume = document.getElementById("stream");
-  volume.volume = vol;
-
   // Sets the new set volume into localstorage
   localStorage.setItem('volumeKey', vol);
 }
